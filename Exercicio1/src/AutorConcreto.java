c32bc5a8641125b89219cf49114ffa8eecf7d511
LeoFC97 <leonardo.fc2@outlook.com>
2019-06-19 17:20:31 -0300

    feature: create AutorConcreto

--- Exercicio1/src/AutorConcreto.java
+++ Exercicio1/src/AutorConcreto.java
@@ -1,9 +1,9 @@
 
-public class AutorConcreto {
+public class AutorConcreto extends Pessoa {
 	private String biografia,nome;
 	
 	public AutorConcreto(long id, String nome,String biografia) {
-		
+		super(id,nome);
 	}
 
 }
